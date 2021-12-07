698f9175e1524eca19545c88b65e94b697582c60
ShiY4 <95706326+ShiY4@users.noreply.github.com>
2021-12-07 22:55:55 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("hello World");
-        System.out.println("second commit");
+        	System.out.println("second commit");
+	    		System.out.println("Changes on GitHub");
     }
 }
