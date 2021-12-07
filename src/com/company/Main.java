6ddee630260675371ce090fb386e0d30c703a613
ShiY4 <shitkin_i@mail.ru>
2021-12-07 22:49:33 +0300

    second committ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
-        System.out.println("Second commit");
+	    System.out.println("hello World");
+        System.out.println("second commit");
     }
 }
